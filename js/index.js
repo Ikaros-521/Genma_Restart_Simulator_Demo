bc22c5698b87d239178390a4a4e882054eb2ff12
Ikaros <327209194@qq.com>
2021-11-20 17:20:03 +0800

    添加手机样式匹配

--- js/index.js
+++ js/index.js
@@ -21,7 +21,48 @@ $(document).ready(function(){
     // $("#prop_page_div").hide();
     // $("#game_page_div").hide();
     // $("#home_page_div").hide();
+    // 调用判断当前访问页面是手机端还是移动端
+    browserRedirect();
 })
+
+// 浏览器css检测匹配
+function browserRedirect() {
+	var sUserAgent = navigator.userAgent.toLowerCase();
+	var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+	var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+	var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+	var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+	var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+	var bIsAndroid = sUserAgent.match(/android/i) == "android";
+	var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+	var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+	if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+		//电脑端
+		//加载css
+		var linkNode = document.createElement("link");
+		linkNode.setAttribute("rel","stylesheet");
+		linkNode.setAttribute("type","text/css");
+		linkNode.setAttribute("href","css/index.css");
+		document.head.appendChild(linkNode);
+		//加载js
+		//var scriptNode = document.createElement("script");
+		//scriptNode.setAttribute("type", "text/javascript");
+		//scriptNode.setAttribute("src", "../../js/index.min.js");
+		//document.head.appendChild(scriptNode);
+	} else {
+		//手机端
+		//加载css
+		var oMeta = document.createElement('meta');
+		oMeta.name = 'viewport';
+		oMeta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no';
+		document.getElementsByTagName('head')[0].appendChild(oMeta);
+		var linkNode = document.createElement("link");
+		linkNode.setAttribute("rel","stylesheet");
+		linkNode.setAttribute("type","text/css");
+		linkNode.setAttribute("href","css/index_phone.css");
+		document.head.appendChild(linkNode);
+	}
+}
 
 // 显示游戏说明
 function show_hide_game_explain_content(type) {
